eaa929ff87ed4bafe04b76c8eff35f436714d4a4
BryanAim <isale.bryan@gmail.com>
2021-08-01 22:49:57 -0700

    fix error

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,34 +1,21 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 /**
- * delete_nodeint_at_index - delete a node at index
- * @head: head of the list
- * @index: location of the node to delete
- * Return: pointer to head of list
+ * add_nodeint - add new nodes to the list
+ * @head: current place in the list
+ * @n: int to add to the list
+ * Return: pointer to current position in list
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	unsigned int i;
-	listint_t *current, *subsequent;
+	listint_t *new;
 
-	if (!head || !*head)
-		return (-1);
-	current = *head;
-	if (index == 0)
-	{
-		*head = (*head)->next;
-		free(current);
-		return (1);
-	}
-	for (i = 0; i < (index - 1); i++)
-	{
-		current = current->next;
-		if (current == NULL)
-			return (-1);
-	}
-	subsequent = current->next;
-	current->next = subsequent->next;
-	free(subsequent);
-	return (1);
+	new = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (*head);
 }
